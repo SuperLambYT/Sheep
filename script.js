2b653f89351295d06849cda6b3b347585c181107
intreet <myEmail@example.com>
2024-05-30 11:24:51 +0300

    second

--- script.js
+++ script.js
@@ -25,4 +25,66 @@ function displayArticles(articles) {
     });
 }
 
-fetchArticles(1).then(displayArticles);
+// Функция для получения информации о статье по ID
+async function fetchArticleById(id) {
+    try {
+        const response = await fetch(`https://gorest.co.in/public-api/posts/${id}`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching article detail:', error);
+        return null;
+    }
+}
+
+// Функция для получения комментариев к статье по ID
+async function fetchCommentsByPostId(postId) {
+    try {
+        const response = await fetch(`https://gorest.co.in/public-api/comments?post_id=${postId}`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching comments:', error);
+        return null;
+    }
+}
+
+// Функция для отображения детальной информации о статье
+async function displayArticleDetail() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const articleId = urlParams.get('id');
+    const articleTitleElement = document.getElementById('article-title');
+    const articleContentElement = document.getElementById('article-content');
+
+    const article = await fetchArticleById(articleId);
+    if (article) {
+        const { title, body } = article.data;
+
+        articleTitleElement.textContent = title;
+        articleContentElement.textContent = body;
+
+        // Получаем и отображаем комментарии к статье
+        const comments = await fetchCommentsByPostId(articleId);
+        if (comments) {
+            const commentsContainer = document.getElementById('comments');
+            commentsContainer.innerHTML = '<h2>Comments</h2>';
+
+            comments.data.forEach(comment => {
+                const commentElement = document.createElement('div');
+                commentElement.innerHTML = `<strong>${comment.name}</strong>: ${comment.body}`;
+                commentsContainer.append(commentElement);
+            });
+        }
+    } else {
+        articleTitleElement.textContent = 'Страница не найдена';
+    }
+}
+
+// Если находимся на странице статьи, отображаем ее детальную информацию
+if (window.location.pathname.includes('post.html')) {
+    displayArticleDetail();
+}
+else {
+    // В противном случае, отображаем список статей
+    fetchArticles(1).then(displayArticles);
+}
